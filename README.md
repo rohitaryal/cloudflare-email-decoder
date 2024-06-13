9d23190a4db8c0923254e904503fcc6b893d8627
rohitaryal <erucix@gmail.com>
2024-06-13 18:16:13 +0530

    Added encoding feature

--- README.md
+++ README.md
@@ -1,26 +1,66 @@
 # CF E-Mail Decoder
-Cloud-flare obfuscates emails generally to prevent from web-scrapers. But there's an easy way out to decode this.
+Cloud-flare obfuscates emails generally to prevent from web-scrapers. But there's an easy way out to decode this. (This can also encode emails if you need)
 
 ## Before beginning:
 - Obfuscated emails are in `data-cfemail` attribute
 - Put the `decode.py` in your library path
 
 ## How to use?
+
+### For decoding:
 ```python
-from decode import decode
+from decoder import decode
 
-decoded_list = decode('73111712001b15101233181a1a075d12105d1a1d')
-
+decoded_list = decode('a7c2dfc6cad7cbc2e7d389cac2')
 print(decoded_list[0])
+```
+
+### For encoding:
+```python
+from decoder import encode
+
+encoded_list = encode('example@gmail.com')
+print(encoded_list[0])
 ```
 
 ## More examples:
  
+ #### Example 1:
 ```python
 from decoder import decode
 
-decoded_list = decode("3e5f54574a554b535f4c4d5f56515110585d5f7e5557574a105f5d105750","73111712001b15101233181a1a075d12105d1a1d")
+decoded_list = decode("a7c2dfc6cad7cbc2e7d389cac2","73111712001b15101233181a1a075d12105d1a1d")
 
 for emails in decoded_list:
     print(emails)
 ```
+#### Example 2:
+
+```python
+from decoder import encode
+
+encoded_list = encode('example@t.me', 'example@a.in')
+
+for strings in encoded_list:
+    print(strings)
+```
+
+#### Example 3:
+
+```python
+from decoder import decode, encode
+
+plain_email = "example@gmail.com"
+
+encoded_email = encode(plain_email)
+decoded_email = decode(encoded_email[0])
+
+print("Testing With: " + plain_email)
+
+print("Re-encoded: " + encoded_email[0])
+print("Re-decoded: " + decoded_email[0])
+
+```
+
+---
+*In case of some issue submit it in issue section*
--- decoder.py
+++ decoder.py
@@ -1,3 +1,5 @@
+import random
+
 def decode(*email_list):
     """
     Returns a list[] of decoded emails.
@@ -6,20 +8,19 @@ def decode(*email_list):
     """
     
     if len(email_list) == 0:
-        raise Exception("No email(s) supplied.")
+        raise Exception("No obfuscated email(s) supplied to decode.")
     
     decoded_email_list = []
         
     for email in email_list:
-        if len(email) < 10:
+        if len(email) < 2:
             raise Exception("Invalid email probably.")
         
         n=2
         final_email=""
         r=int(email[:n], base=16) #Hexadecimal
         
-        
-        while len(email) - n > 0:
+        while len(email) - n > 0: # Other exception may be generated here
             n += 2
             ascii_value=int(email[n-2:n], base=16) ^ r
             final_email += chr(ascii_value)
@@ -27,3 +28,56 @@ def decode(*email_list):
         decoded_email_list.append(final_email)
             
     return decoded_email_list
+
+def get_random_r():
+    min = 26  # 0x1a
+    max = 255 # 0xff
+    random_number = random.randrange(min, max + 1) # max and min inclusive
+
+    return random_number
+
+def encode(*email_list):
+    """
+    Returns a list[] of encoded emails.
+    Will throw `Exception` in case of empty
+    email(s) supplied
+    """
+
+    if len(email_list) == 0:
+        raise Exception("No email(s) supplied to encode.")
+
+    encoded_email_list = []
+
+    for email in email_list:
+        r = get_random_r()
+        encoded_email = hex(r)[2:] # Since first 2 letters is the decoder key (r)
+
+        for char in email:  # Other exception may be generated here
+            ascii_code = ord(char)
+            ascii_code ^= r
+            hex_code = hex(ascii_code)[2:]
+
+            # some hex might be less than 10 but we want pair always
+            # so append 0 to number less than 10
+            hex_string = '0' + hex_code if len(hex_code) < 2 else hex_code
+
+            encoded_email += hex_string
+
+        encoded_email_list.append(encoded_email)
+
+    return encoded_email_list
+
+if __name__ == "__main__":
+    print("[1] Encode")
+    print("[2] Decode")
+    option = int(input("CHOOSE: "))
+
+    if option == 1:
+        while True:
+            print(encode(input("E-Mail: ")))
+    elif option == 2:
+        while True:
+            print(decode(input("String: ")))
+    else:
+        print("[!] Invalid option")
+        exit(0)
